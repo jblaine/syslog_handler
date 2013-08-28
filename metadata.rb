74a43913e136107ee817f986125cf8b930342674
Jeff Blaine <jblaine@kickflop.net>
2013-08-28 10:35:42 -0400

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jblaine@mitre.org'
 license          'All rights reserved'
 description      'Installs/Configures syslog_handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
